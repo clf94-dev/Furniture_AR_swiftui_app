bb6b8f20435f2bd6524c18c1997b559feab310aa
clf94-dev <clf94@hotmail.es>
2023-08-26 08:36:31 +0200

    Initial Commit

---
+++ Furniture_AR_swiftui_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Furniture_AR_swiftui_app
+//
+//  Created by Carmen Lucas on 26/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Furniture_AR_swiftui_app/Furniture_AR_swiftui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_AR_swiftui_appApp.swift
+//  Furniture_AR_swiftui_app
+//
+//  Created by Carmen Lucas on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_AR_swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
